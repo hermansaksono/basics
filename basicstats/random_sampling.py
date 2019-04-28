297c2ba32282bd863bcc5034de626c1a8a0fd266
hermansaksono <herman.saksono@gmail.com>
2019-04-28 13:18:31 -0400

    Make the codes more clear

--- basicstats/random_sampling.py
+++ basicstats/random_sampling.py
@@ -6,8 +6,8 @@ apple_percent = 56.0
 banana_percent = 44.0
 
 # Creating the population
-apples = [0] * int(apple_percent * total_fruits)  # Apples population
-bananas = [1] * int(banana_percent * total_fruits)  # Bananas population
+apples = [0] * int(apple_percent * total_fruits)  # Apple population
+bananas = [1] * int(banana_percent * total_fruits)  # Banana population
 
 apples_plus_bananas = apples + bananas
 random.shuffle(apples_plus_bananas)
@@ -17,13 +17,14 @@ random.shuffle(apples_plus_bananas)
 num_samples = 200
 random_picks = [i for i in range(0, total_fruits)]
 random.shuffle(random_picks)
+samples_to_be_picked = random_picks[0:num_samples]
 
 
 # Sampling the fruits
 apples_sampled = 0
 bananas_sampled = 0
 
-for i in random_picks[0:num_samples]:
+for i in random_picks[0:samples_to_be_picked]:
     if apples_plus_bananas[i] == 0:
         apples_sampled += 1
     if apples_plus_bananas[i] == 1:
